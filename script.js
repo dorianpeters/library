aae9673d3ff2dd057a6c8874ab4dfb03c3677cf3
Dorian Peters <dpeters08@gmail.com>
2024-04-13 15:10:19 -0700

    Add seed data to display, add displayBooks function to show books, removed blank book divs

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const myLibrary = [];
+// Starter data
+const myLibrary = [
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garcia Marquez",
+    pages: 100,
+    read: true,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    pages: 200,
+    read: false,
+  },
+];
+
+// Main function
+myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
+console.table(myLibrary);
+
+const content = document.querySelector(".content");
 const openButton = document.querySelector("[add-book-btn]");
 const closeButton = document.querySelector("[close-modal-btn]");
 const modal = document.querySelector("[book-modal]");
+
+displayBooks();
+
+// Arrow functions
+openButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
+
+// Function declarations
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -21,14 +54,15 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {}
 
-function displayBooks() {}
+function displayBooks() {
+  for (const book of myLibrary) {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    const bookText = document.createTextNode(
+      `${book.title} by ${book.author}, ${book.pages} pages`
+    );
 
-openButton.addEventListener("click", () => {
-  modal.showModal();
-});
-
-closeButton.addEventListener("click", () => {
-  modal.close();
-});
-let newBook = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-console.log(newBook.info());
+    bookDiv.appendChild(bookText);
+    content.appendChild(bookDiv);
+  }
+}
