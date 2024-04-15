5ff20c99dc0586c5d9fc4da6f5842ccdc45468d3
Dorian Peters <dpeters08@gmail.com>
2024-04-14 23:02:42 -0700

    Added index to book object, seperated title author and pages into seperate p elements, added style

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// Starter data
-const myLibrary = [
-  {
-    title: "One Hundred Years of Solitude",
-    author: "Gabriel Garcia Marquez",
-    pages: 100,
-    read: true,
-  },
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    pages: 200,
-    read: false,
-  },
-];
+// // Starter data
+// const myLibrary = [
+//   {
+//     title: "One Hundred Years of Solitude",
+//     author: "Gabriel Garcia Marquez",
+//     pages: 100,
+//     read: true,
+//   },
+//   {
+//     title: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     pages: 200,
+//     read: false,
+//   },
+// ];
 
 // Main function
+const myLibrary = [];
 myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
 console.table(myLibrary);
 
@@ -26,6 +27,7 @@ const modal = document.querySelector("[book-modal]");
 const form = document.querySelector("form");
 
 displayBooks();
+let removeButtons = document.querySelectorAll(".books button");
 
 // Arrow functions
 openButton.addEventListener("click", () => {
@@ -40,9 +42,9 @@ submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   const title = form.elements["title"].value;
   const author = form.elements["author"].value;
-  const pages = form.elements["pages"].value;
-  const read = form.elements["read"].value;
-  console.log(read);
+  const pages = parseInt(form.elements["pages"].value);
+  let strRead = form.elements["read"].value;
+  const read = strRead.toLowerCase() == "true" ? true : false;
   myLibrary.push(new Book(title, author, pages, read));
   content.replaceChildren();
   displayBooks();
@@ -55,6 +57,7 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.index = myLibrary.length;
 }
 
 function addBookToLibrary() {}
@@ -63,10 +66,25 @@ function displayBooks() {
   for (const book of myLibrary) {
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
-    const bookText = document.createTextNode(
-      `${book.title} by ${book.author}, ${book.pages} pages`
-    );
-    bookDiv.appendChild(bookText);
+
+    const titleP = document.createElement("p");
+    const titleText = document.createTextNode(`${book.title}`);
+    const authorP = document.createElement("p");
+    const authorText = document.createTextNode(`${book.author}`);
+    const pagesP = document.createElement("p");
+    const pagesText = document.createTextNode(`${book.pages} pages`);
+    const removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "Remove";
+
+    titleP.appendChild(titleText);
+    authorP.appendChild(authorText);
+    pagesP.appendChild(pagesText);
+
+    bookDiv.appendChild(titleP);
+    bookDiv.appendChild(authorP);
+    bookDiv.appendChild(pagesP);
+    bookDiv.appendChild(removeBtn);
+
     content.appendChild(bookDiv);
   }
 }
